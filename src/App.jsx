f7faa74a6141190dcdb77ca43f53a3a9b0e4851e
Kyle Trinh <kyle.trinh94@gmail.com>
2023-12-06 17:17:33 -0800

    :sparkles: Add navbar and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
+import { Suspense } from 'react';
+import StarsCanvas from './components/canvas/StarsCanvas';
+import { navLinks } from './constants';
+import EarthCanvas from './components/canvas/EarthCanvas';
+
 function App() {
   return (
-    <>
-      <h1>Minh Tran Portfolio</h1>
-    </>
+    <Suspense fallback={null}>
+      <StarsCanvas />
+      <nav>
+        <ul className='flex justify-center gap-12 uppercase py-6 text-2xl'>
+          {navLinks.map(({ title, id }) => (
+            <li key={id} className='hover:text-secondary'>
+              <a href={`/#${id}`}>{title}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className='h-screen text-white'>
+        <div className='flex justify-center h-full w-full container mx-auto items-center overflow-hidden flex-col xl:flex-row'>
+          <div className='xl:flex-1 xl:h-[850px] md:h-[550px] h-[350px]'>
+            <EarthCanvas />
+          </div>
+          <div className='flex-1 ml-10 text-center xl:text-left'>
+            <h1 className='text-7xl mb-6'>MINH TRAN</h1>
+            <div className='flex flex-col gap-6 font-light text-lg'>
+              <p>
+                I am a graduate student of the Master of Science in Computer
+                Vision at Carnegie Mellon University, where I work on computer
+                vision and machine learning.
+              </p>
+              <p>
+                Prior to CMU, I've worked at Actuate AI as a Data Scientist for
+                more than 3 years. Prior to Actuate, I've earned my degree in
+                Data Science from DePaul University, where I was advised by
+                Prof.Jacob Furst, and in E-commerce from Foreign Trade
+                University of Hanoi, advised by Prof.Hung Nguyen.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='h-screen text-white'>Section 1</div>
+      <div className='h-screen text-white'>Section 1</div>
+    </Suspense>
   );
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: 'research-interest',
+    title: 'Research Interest',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'honors',
+    title: 'Honors & Awards',
+  },
+  {
+    id: 'courseworks',
+    title: 'Related Courseworks',
+  },
+];
